5816e77795c05aecbfec2f9d1ac17b6d468b9b04
ayushwasnothere <ayushwasnothere@gmail.com>
2025-03-31 06:26:08 +0530

    fix quote again

--- packages/ui/src/quote.tsx
+++ packages/ui/src/quote.tsx
@@ -9,13 +9,15 @@ export const Quote = ({ quote, author, authorDescription }: QuoteProps) => {
     <div className="hidden w-full bg-gray-100 pl-20 pr-10 xl:pl-40 xl:pr-20 lg:flex justify-center">
       <div className="flex flex-col justify-center gap-4">
         <div className="font-bold text-3xl">
+          "
           {quote ||
             "Like a raven’s call in the silent night, a message carries both mystery and meaning."}
+          "
         </div>
         <div>
           <div className="font-bold text-xl">{author || "The Raven"}</div>
           <div className="text-slate-400 text-md">
-            {authorDescription || ""}
+            {authorDescription || "Unknown"}
           </div>
         </div>
       </div>
